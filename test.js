a25d5f011de2b071053d9b520e68bb40635519ec
Mika215 Michael <mikalsoyra@gmail.com>
2021-09-23 12:16:42 +0200

    pusing excercise one 1.1-1.6

---
+++ test.js
@@ -0,0 +1,13 @@
+console.log("It works !!!");
+console.log("hello");
+
+const readlineSync = require("readline-sync");
+let userName = readlineSync.question('Can you give me your name please?');
+console.log("Hello " + userName);
+
+
+let age = 25; // I'm 25
+age = 36; // I get older
+age = 18; // Miraculously I'm getting younger
+
+ 
